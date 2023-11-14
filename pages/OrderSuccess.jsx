94ee7811caf037a1cba0a9c96c7d89eadb43db72
AkshataShanmugam <81082573+AkshataShanmugam@users.noreply.github.com>
2023-11-15 02:34:17 +0530

    Update OrderSuccess.jsx

--- pages/OrderSuccess.jsx
+++ pages/OrderSuccess.jsx
@@ -9,12 +9,17 @@ const OrderSuccess = () => {
     navigate("/");
   };
 
+  const CheckOut = () => {
+    navigate("/checkOut");
+  };
+
   return (
     <div className="order-success-container">
       <h1> Your order is confirmed! </h1>
       <h2> Your order is getting prepared by the top chefs of the town, and you will be served hot food soon. </h2>
       <img src ={animation} alt = "GIF of burger being being assembled" />
       <button className="toOrder--links" onClick={home}>Return Home</button>
+      <button className="toOrder--links" onClick={CheckOut}>See summary of orders</button>
     </div>
   );
 }
