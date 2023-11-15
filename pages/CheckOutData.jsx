f0c43b221c77ae7491ea3d66aba3ec557f620014
AkshataShanmugam <81082573+AkshataShanmugam@users.noreply.github.com>
2023-11-15 19:36:59 +0530

    Update CheckOutData.jsx

--- pages/CheckOutData.jsx
+++ pages/CheckOutData.jsx
@@ -1,36 +1,40 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-import React from "react";
+import React from 'react'
 
-let mylist = [];
+const CheckOutData = () => {
+    let mylist = [];
 
-const appSettings = {
-    databaseURL: "https://restaurant-management-v18-default-rtdb.firebaseio.com/"
-};
+    const appSettings = {
+        databaseURL: "https://restaurant-management-v18-default-rtdb.firebaseio.com/"
+    };
 
+    const app = initializeApp(appSettings);
+    const database = getDatabase(app);
+    const menuItemsInDB = ref(database, "menuItems");
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const menuItemsInDB = ref(database, "menuItems");
+    const storedItems = JSON.parse(localStorage.getItem('emailId')) || {};
+    const props = storedItems; 
 
-const storedItems = JSON.parse(localStorage.getItem('emailId')) || {};
-const props = storedItems; 
-
-onValue(menuItemsInDB, function(snapshot) {
-    let menuItemsArray = Object.values(snapshot.val())
-    
-    for (let i = 0; i < menuItemsArray.length; i++) {
-        let currentItem= menuItemsArray[i];
-        let emailId = Object.keys(currentItem);
-        emailId = emailId[0];
-        emailId = emailId.replace(/\*/g, ".");
-        if (emailId === props){
-            let temp = Object.values(currentItem)[0][0]
-            mylist.push(temp);
-            console.log("this?:", mylist);
+    onValue(menuItemsInDB, function(snapshot) {
+        let menuItemsArray = Object.values(snapshot.val())
+        console.log("email curr in checkout details jsx ", props);
+        for (let i = 0; i < menuItemsArray.length; i++) {
+            let currentItem= menuItemsArray[i];
+            let emailId = Object.keys(currentItem);
+            emailId = emailId[0];
+            emailId = emailId.replace(/\*/g, ".");
+            if (emailId === props){
+                let temp = Object.values(currentItem)[0][0]
+                mylist.push(temp);
+                console.log("this?:", mylist);
+            }
         }
-    }
-})
+    })
 
-export default mylist;
+    localStorage.setItem('checkOutItems', JSON.stringify(mylist));
+    return (mylist)
+}
+
+export default CheckOutData
