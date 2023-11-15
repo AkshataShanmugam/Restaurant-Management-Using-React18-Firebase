fe90f6f4c50ca278e73ab717fbc06093cde37b00
AkshataShanmugam <81082573+AkshataShanmugam@users.noreply.github.com>
2023-11-16 04:13:40 +0530

    Update CheckOutData.jsx

--- pages/CheckOutData.jsx
+++ pages/CheckOutData.jsx
@@ -1,49 +1,57 @@
 import { useState, useEffect } from 'react';
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
 const CheckOutData = () => {
-    const [mylist, setMyList] = useState([]);
-    localStorage.setItem('checkOutItems', JSON.stringify(mylist));
+  const [mylist, setMyList] = useState([]);
+  const storedItems = JSON.parse(localStorage.getItem('emailId')) || {};
+  const props = storedItems;
 
-    const appSettings = {
-        databaseURL: "https://restaurant-management-v18-default-rtdb.firebaseio.com/"
+  const appSettings = {
+    databaseURL: 'https://restaurant-management-v18-default-rtdb.firebaseio.com/',
+  };
+
+  const app = initializeApp(appSettings);
+  const database = getDatabase(app);
+  const menuItemsInDB = ref(database, 'menuItems');
+
+  useEffect(() => {
+    const fetchData = () => {
+      onValue(menuItemsInDB, function (snapshot) {
+        let menuItemsArray = Object.values(snapshot.val());
+        let newList = [];
+        for (let i = 0; i < menuItemsArray.length; i++) {
+          let currentItem = menuItemsArray[i];
+          let emailId = Object.keys(currentItem);
+          emailId = emailId[0];
+          emailId = emailId.replace(/\*/g, '.');
+          if (emailId === props) {
+            let temp = Object.values(currentItem)[0][0];
+            newList.push(temp);
+          }
+        }
+        setMyList(newList);
+      });
     };
 
-    const app = initializeApp(appSettings);
-    const database = getDatabase(app);
-    const menuItemsInDB = ref(database, "menuItems");
+    // Fetch data initially
+    fetchData();
 
-    const storedItems = JSON.parse(localStorage.getItem('emailId')) || {};
-    const props = storedItems;
+    // Set up interval to fetch data every 1 second
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 2000);
 
-    useEffect(() => {
-        const fetchData = () => {
-            onValue(menuItemsInDB, function (snapshot) {
-                let menuItemsArray = Object.values(snapshot.val());
-                let newList = [];
-                for (let i = 0; i < menuItemsArray.length; i++) {
-                    let currentItem = menuItemsArray[i];
-                    let emailId = Object.keys(currentItem);
-                    emailId = emailId[0];
-                    emailId = emailId.replace(/\*/g, ".");
-                    if (emailId === props) {
-                        let temp = Object.values(currentItem)[0][0];
-                        newList.push(temp);
-                    }
-                }
-                setMyList(newList);
-            });
-        };
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
-        fetchData();
-    }, [menuItemsInDB, props]);
-
-
+  // Update local storage whenever the list changes
+  useEffect(() => {
     localStorage.setItem('checkOutItems', JSON.stringify(mylist));
-    
-    return (mylist)
-    // <div>CheckOutData</div>
-}
+  }, [mylist]);
 
-export default CheckOutData
+  return mylist;
+};
+
+export default CheckOutData;
