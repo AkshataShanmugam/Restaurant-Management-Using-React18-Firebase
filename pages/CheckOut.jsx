3dfa05a0dd25f99d2c34cb47f622fc88e2b60e94
AkshataShanmugam <81082573+AkshataShanmugam@users.noreply.github.com>
2023-11-15 19:37:56 +0530

    Update CheckOut.jsx

--- pages/CheckOut.jsx
+++ pages/CheckOut.jsx
@@ -1,3 +1,132 @@
+import SpecialDishes from "./CheckOutOrders.jsx"
+import Data from "./CheckOutData.jsx"
+
+import React, {useState} from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import logo from "../src/images/logo.png";
+
+const CheckOut = () => {
+    const navigate = useNavigate();
+
+    const [selectedOption, setSelectedOption] = useState("");
+    const handleOptionChange = (event) => {
+        const selectedValue = event.target.value;
+        setSelectedOption(selectedValue);
+        navigate(selectedValue);
+    };
+
+    const returnToPage = () => {
+        navigate("/")
+    }
+    
+    function flattenDictionary(dict) {
+        const result = [];
+        
+        for (const key1 in dict) {
+            const level1 = dict[key1];
+            for (const key2 in level1) {
+                const level2 = level1[key2];
+                if (level2.hasOwnProperty("id") && level2.hasOwnProperty("title")) {
+                    result.push({ "id": level2.id, "title": level2.title, "count": level2.count, "coverImg": level2.coverImg, "description":level2.description, "price":level2.price });
+                } else {
+                    for (const key3 in level2) {
+                        const level3 = level2[key3];
+                        result.push({ "id": level3.id, "title": level3.title, "count": level3.count, "coverImg": level3.coverImg, "description":level3.description, "price":level3.price });
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+
+    const data = JSON.parse(localStorage.getItem('checkOutItems')) || [];
+    console.log("data", data);
+
+    const temp = flattenDictionary(Object.values(Object.values(data)));
+    console.log("temp:", temp);
+    console.log("special", temp.length);
+    if (temp.length === 0){
+        return (
+            <div>
+                <nav>
+                    <div className='nav--div'>
+                        <img src={logo} className="nav--logo" />
+                        <div className="nav--components">
+                        <select
+                            onChange={handleOptionChange}
+                            value={selectedOption}
+                            className="nav--select"
+                        >
+                            <option value="" disabled hidden>
+                            More Options
+                            </option>
+                            <option value="/">HOME</option>
+                            <option value="/logout">LOG OUT</option>
+                            <option value="/placeOrder">PLACE ORDERS</option>
+                        </select>
+                        </div>
+                    </div>
+                </nav>
+                <h1 className="checkOut--null-text">Let's order some food!</h1>
+                <button className="button--navigate" onClick={returnToPage}> RETURN HOME </button>
+            </div>
+        )
+
+    } else {
+        console.log("umm", flattenDictionary(Object.values(Object.values(data))));
+        console.log("curr", Object.values(data[0]))
+        const handleOptionChange = (event) => {
+            const selectedValue = event.target.value;
+            setSelectedOption(selectedValue);
+            navigate(selectedValue);
+        };
+
+        const returnToPage = () => {
+            navigate("/");
+        };
+        
+        const specials = (temp).map(item => {
+            return (
+                <SpecialDishes
+                    key={item.id}
+                    item={item}
+                />
+            )
+        }) 
+
+        return (
+            <div>
+                <nav>
+                    <div className='nav--div'>
+                        <img src={logo} className="nav--logo" />
+                        <div className="nav--components">
+                        <select
+                            onChange={handleOptionChange}
+                            value={selectedOption}
+                            className="nav--select"
+                        >
+                            <option value="" disabled hidden>
+                            More Options
+                            </option>
+                            <option value="/">HOME</option>
+                            <option value="/logout">LOG OUT</option>
+                        </select>
+                        </div>
+                    </div>
+                </nav>
+                <section className="cards-list">
+                    {specials}
+                </section>
+                <button className="button--navigate" onClick={returnToPage}> RETURN HOME </button>
+            </div>
+        )
+    }
+}
+
+export default CheckOut
+// V1 :
 // import SpecialDishes from "./CheckOutOrders.jsx"
 // import data from "./CheckOutData.jsx"
 
@@ -107,132 +236,133 @@
 // export default CheckOut
 
 
-import SpecialDishes from "./CheckOutOrders.jsx"
-import data from "./CheckOutData.jsx"
+// V2:
+// import SpecialDishes from "./CheckOutOrders.jsx"
+// import data from "./CheckOutData.jsx"
 
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 
-import { useNavigate } from 'react-router-dom';
-import logo from "../src/images/logo.png";
+// import { useNavigate } from 'react-router-dom';
+// import logo from "../src/images/logo.png";
 
-import GetSignedInEmail from "./SignInCheck.jsx"
+// import GetSignedInEmail from "./SignInCheck.jsx"
 
-const CheckOut = () => {
-    const navigate = useNavigate();
+// const CheckOut = () => {
+//     const navigate = useNavigate();
 
-    const [selectedOption, setSelectedOption] = useState("");
-    const handleOptionChange = (event) => {
-        const selectedValue = event.target.value;
-        setSelectedOption(selectedValue);
-        navigate(selectedValue);
-    };
+//     const [selectedOption, setSelectedOption] = useState("");
+//     const handleOptionChange = (event) => {
+//         const selectedValue = event.target.value;
+//         setSelectedOption(selectedValue);
+//         navigate(selectedValue);
+//     };
 
-    const returnToPage = () => {
-        navigate("/")
-    }
+//     const returnToPage = () => {
+//         navigate("/")
+//     }
 
-    console.log("attempt", Object.values(Object.values(data)));
+//     console.log("attempt", Object.values(Object.values(data)));
     
-    function flattenDictionary(dict) {
-        const result = [];
+//     function flattenDictionary(dict) {
+//         const result = [];
         
-        for (const key1 in dict) {
-            const level1 = dict[key1];
-            for (const key2 in level1) {
-                const level2 = level1[key2];
-                if (level2.hasOwnProperty("id") && level2.hasOwnProperty("title")) {
-                    result.push({ "id": level2.id, "title": level2.title, "count": level2.count, "coverImg": level2.coverImg, "description":level2.description, "price":level2.price });
-                } else {
-                    for (const key3 in level2) {
-                        const level3 = level2[key3];
-                        result.push({ "id": level3.id, "title": level3.title, "count": level3.count, "coverImg": level3.coverImg, "description":level3.description, "price":level3.price });
-                    }
-                }
-            }
-        }
+//         for (const key1 in dict) {
+//             const level1 = dict[key1];
+//             for (const key2 in level1) {
+//                 const level2 = level1[key2];
+//                 if (level2.hasOwnProperty("id") && level2.hasOwnProperty("title")) {
+//                     result.push({ "id": level2.id, "title": level2.title, "count": level2.count, "coverImg": level2.coverImg, "description":level2.description, "price":level2.price });
+//                 } else {
+//                     for (const key3 in level2) {
+//                         const level3 = level2[key3];
+//                         result.push({ "id": level3.id, "title": level3.title, "count": level3.count, "coverImg": level3.coverImg, "description":level3.description, "price":level3.price });
+//                     }
+//                 }
+//             }
+//         }
         
-        return result;
-    }
+//         return result;
+//     }
 
-    const temp = flattenDictionary(Object.values(Object.values(data)));
-    console.log("special", temp.length);
-    if (temp.length === 0){
-        return (
-            <div>
-                <nav>
-                    <div className='nav--div'>
-                        <img src={logo} className="nav--logo" />
-                        <div className="nav--components">
-                        <select
-                            onChange={handleOptionChange}
-                            value={selectedOption}
-                            className="nav--select"
-                        >
-                            <option value="" disabled hidden>
-                            More Options
-                            </option>
-                            <option value="/">HOME</option>
-                            <option value="/logout">LOG OUT</option>
-                            <option value="/placeOrder">PLACE ORDERS</option>
-                        </select>
-                        </div>
-                    </div>
-                </nav>
-                <h1 className="checkOut--null-text">Let's order some food!</h1>
-                <button className="button--navigate" onClick={returnToPage}> RETURN HOME </button>
-            </div>
-        )
+//     const temp = flattenDictionary(Object.values(Object.values(data)));
+//     console.log("special", temp.length);
+//     if (temp.length === 0){
+//         return (
+//             <div>
+//                 <nav>
+//                     <div className='nav--div'>
+//                         <img src={logo} className="nav--logo" />
+//                         <div className="nav--components">
+//                         <select
+//                             onChange={handleOptionChange}
+//                             value={selectedOption}
+//                             className="nav--select"
+//                         >
+//                             <option value="" disabled hidden>
+//                             More Options
+//                             </option>
+//                             <option value="/">HOME</option>
+//                             <option value="/logout">LOG OUT</option>
+//                             <option value="/placeOrder">PLACE ORDERS</option>
+//                         </select>
+//                         </div>
+//                     </div>
+//                 </nav>
+//                 <h1 className="checkOut--null-text">Let's order some food!</h1>
+//                 <button className="button--navigate" onClick={returnToPage}> RETURN HOME </button>
+//             </div>
+//         )
 
-    } else {
-        console.log("umm", flattenDictionary(Object.values(Object.values(data))));
-        console.log("curr", Object.values(data[0]))
-        const handleOptionChange = (event) => {
-            const selectedValue = event.target.value;
-            setSelectedOption(selectedValue);
-            navigate(selectedValue);
-        };
+//     } else {
+//         console.log("umm", flattenDictionary(Object.values(Object.values(data))));
+//         console.log("curr", Object.values(data[0]))
+//         const handleOptionChange = (event) => {
+//             const selectedValue = event.target.value;
+//             setSelectedOption(selectedValue);
+//             navigate(selectedValue);
+//         };
 
-        const returnToPage = () => {
-            navigate("/");
-        };
+//         const returnToPage = () => {
+//             navigate("/");
+//         };
         
-        const specials = (temp).map(item => {
-            return (
-                <SpecialDishes
-                    key={item.id}
-                    item={item}
-                />
-            )
-        }) 
+//         const specials = (temp).map(item => {
+//             return (
+//                 <SpecialDishes
+//                     key={item.id}
+//                     item={item}
+//                 />
+//             )
+//         }) 
 
-        return (
-            <div>
-                <nav>
-                    <div className='nav--div'>
-                        <img src={logo} className="nav--logo" />
-                        <div className="nav--components">
-                        <select
-                            onChange={handleOptionChange}
-                            value={selectedOption}
-                            className="nav--select"
-                        >
-                            <option value="" disabled hidden>
-                            More Options
-                            </option>
-                            <option value="/">HOME</option>
-                            <option value="/logout">LOG OUT</option>
-                        </select>
-                        </div>
-                    </div>
-                </nav>
-                <section className="cards-list">
-                    {specials}
-                </section>
-                <button className="button--navigate" onClick={returnToPage}> RETURN HOME </button>
-            </div>
-        )
+//         return (
+//             <div>
+//                 <nav>
+//                     <div className='nav--div'>
+//                         <img src={logo} className="nav--logo" />
+//                         <div className="nav--components">
+//                         <select
+//                             onChange={handleOptionChange}
+//                             value={selectedOption}
+//                             className="nav--select"
+//                         >
+//                             <option value="" disabled hidden>
+//                             More Options
+//                             </option>
+//                             <option value="/">HOME</option>
+//                             <option value="/logout">LOG OUT</option>
+//                         </select>
+//                         </div>
+//                     </div>
+//                 </nav>
+//                 <section className="cards-list">
+//                     {specials}
+//                 </section>
+//                 <button className="button--navigate" onClick={returnToPage}> RETURN HOME </button>
+//             </div>
+//         )
 
-    }
-}
+//     }
+// }
 
-export default CheckOut
+// export default CheckOut
