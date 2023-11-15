9a16797556d3f18befa3b677d7fee7149eaeb586
AkshataShanmugam <81082573+AkshataShanmugam@users.noreply.github.com>
2023-11-16 02:49:39 +0530

    Update CheckOutData.jsx

--- pages/CheckOutData.jsx
+++ pages/CheckOutData.jsx
@@ -1,11 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const CheckOutData = () => {
-    let mylist = JSON.parse(localStorage.getItem('checkOutItems')) || [];
-    
+    const [mylist, setMyList] = useState([]);
+    localStorage.setItem('checkOutItems', JSON.stringify(mylist));
+
     const appSettings = {
         databaseURL: "https://restaurant-management-v18-default-rtdb.firebaseio.com/"
     };
@@ -15,27 +15,35 @@ const CheckOutData = () => {
     const menuItemsInDB = ref(database, "menuItems");
 
     const storedItems = JSON.parse(localStorage.getItem('emailId')) || {};
-    const props = storedItems; 
+    const props = storedItems;
 
-    onValue(menuItemsInDB, function(snapshot) {
-        let menuItemsArray = Object.values(snapshot.val())
-        console.log("email curr in checkout details jsx ", props);
-        for (let i = 0; i < menuItemsArray.length; i++) {
-            let currentItem= menuItemsArray[i];
-            let emailId = Object.keys(currentItem);
-            emailId = emailId[0];
-            emailId = emailId.replace(/\*/g, ".");
-            if (emailId === props){
-                let temp = Object.values(currentItem)[0][0]
-                mylist.push(temp);
-                console.log("this?:", mylist);
-            }
-        }
-    })
+    useEffect(() => {
+        const fetchData = () => {
+            onValue(menuItemsInDB, function (snapshot) {
+                let menuItemsArray = Object.values(snapshot.val());
+                let newList = [];
+                for (let i = 0; i < menuItemsArray.length; i++) {
+                    let currentItem = menuItemsArray[i];
+                    let emailId = Object.keys(currentItem);
+                    emailId = emailId[0];
+                    emailId = emailId.replace(/\*/g, ".");
+                    if (emailId === props) {
+                        let temp = Object.values(currentItem)[0][0];
+                        newList.push(temp);
+                    }
+                }
+                setMyList(newList);
+            });
+        };
 
-    localStorage.setItem('checkOutItems', JSON.stringify(mylist));
+        fetchData();
+    }, [menuItemsInDB, props]);
+
+
+    // localStorage.setItem('checkOutItems', JSON.stringify(mylist));
     
     return (mylist)
+    // <div>CheckOutData</div>
 }
 
 export default CheckOutData
